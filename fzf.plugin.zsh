ee4c2a6162183b3afacd99ba73844fa96681ab5f
Christian Chiarulli <chris.machine@pm.me>
2022-11-10 19:41:47 -0500

    feat: source fzf bindings and stuff

---
+++ fzf.plugin.zsh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+[ -f /usr/share/fzf/completion.zsh ] && source /usr/share/fzf/completion.zsh
+[ -f /usr/share/fzf/key-bindings.zsh ] && source /usr/share/fzf/key-bindings.zsh
+[ -f /usr/share/doc/fzf/examples/completion.zsh ] && source /usr/share/doc/fzf/examples/completion.zsh
+[ -f /usr/share/doc/fzf/examples/key-bindings.zsh ] && source /usr/share/doc/fzf/examples/key-bindings.zsh
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
