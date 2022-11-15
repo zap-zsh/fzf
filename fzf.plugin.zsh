5e40bc5392aa2e9b2a2b7c619a6f931451c95186
Brett Chik <87502602+bdchik@users.noreply.github.com>
2022-11-14 20:16:15 -0800

    Add support for searching MacOS Homebrew locations

--- fzf.plugin.zsh
+++ fzf.plugin.zsh
@@ -5,3 +5,9 @@
 [ -f /usr/share/doc/fzf/examples/completion.zsh ] && source /usr/share/doc/fzf/examples/completion.zsh
 [ -f /usr/share/doc/fzf/examples/key-bindings.zsh ] && source /usr/share/doc/fzf/examples/key-bindings.zsh
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+if [[ "$(uname -s)" == "Darwin" && "$(command -v brew)" != "" ]] ; then
+  local FZF_HOME=$(brew --prefix)/opt/fzf
+  [ -f $FZF_HOME/shell/completion.zsh ] && source $FZF_HOME/shell/completion.zsh
+  [ -f $FZF_HOME/shell/key-bindings.zsh ] && source $FZF_HOME/shell/key-bindings.zsh
+fi
